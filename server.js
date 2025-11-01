db9a942396e0cf4f1983a86e5b85a22f2cb881a7
VkyM <vicky.nimdasys@gmail.com>
2025-11-01 14:28:34 +0530

    Add .gitignore to exclude node_modules and package-lock.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
